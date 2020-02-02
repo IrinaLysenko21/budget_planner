cbac0b0e8b9e09b61f5e302b5b40c9c17b851553
Irina Lysenko <irina.lysenko21@gmail.com>
2020-02-02 22:51:02 +0200

    Updates

---
+++ precache-manifest.16a9fc93451d9b412fdc419164c48415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4267b622953f5783c80b4c7afe2e45",
+    "url": "/budget_planner/index.html"
+  },
+  {
+    "revision": "6e4cd1c8025d47516b96",
+    "url": "/budget_planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "a21142f80fa68fa61b7a",
+    "url": "/budget_planner/static/js/2.132f83e5.chunk.js"
+  },
+  {
+    "revision": "6e4cd1c8025d47516b96",
+    "url": "/budget_planner/static/js/main.8c23644c.chunk.js"
+  },
+  {
+    "revision": "b5c6b52538704e5fbb63",
+    "url": "/budget_planner/static/js/runtime-main.aafb7ef5.js"
+  }
+]);
